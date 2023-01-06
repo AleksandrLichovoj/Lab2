ebdb7e142bb9eb9fa633158415dbe460cc2f96c2
AleksandrLichovoj <117812561+AleksandrLichovoj@users.noreply.github.com>
2023-01-06 21:18:25 +0200

    Add files via upload

---
+++ software.design/AppTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+public class AppTest
+{
+
+    [Fact]
+    public void MaxSym()
+    {
+        string paintingid = "1";
+        string paintname = "reallylongassnameidkaefgaeirgbrtngbregruignuiwrgnurtqgnuirthrnhwnurhsorhotgtrhgtrthrin";
+        string author = "some dude";
+        string country = "Earth";
+        string year = "XXXX";
+    }
+
+    [Fact]
+    public void MinSym()
+    {
+        string paintingid = "2";
+        string paintname = "X";
+        string author = "some dude";
+        string country = "Earth";
+        string year = "XXXX";
+    }
+}
